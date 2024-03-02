d47f07a058f79abf8367076cd6ab0b9892e144b2
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-03-02 10:57:35 +0200

    best  seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bokkly_app/core/utils/assets.dart';
 import 'package:bokkly_app/core/utils/styles.dart';
 import 'package:bokkly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bokkly_app/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -16,9 +17,42 @@ class HomeViewBody extends StatelessWidget {
         FeaturedBooksListView(),
         SizedBox(height: 50),
         Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Text('Best Seller', style: Styles.titleMedium))
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text('Best Seller', style: Styles.titleMedium),
+        ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
+        ],
+      ),
     );
   }
 }
