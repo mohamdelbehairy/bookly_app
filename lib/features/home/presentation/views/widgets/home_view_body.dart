c289a0fdeee0715d0897a520201f72c0b826d8d7
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-03-03 04:56:40 +0200

    best seller item part three

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
-import 'package:bokkly_app/core/utils/assets.dart';
 import 'package:bokkly_app/core/utils/styles.dart';
+import 'package:bokkly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bokkly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bokkly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -22,49 +22,6 @@ class HomeViewBody extends StatelessWidget {
         SizedBox(height: 20),
         BestSellerListViewItem(),
       ],
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SizedBox(
-        height: 125,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red,
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(AssetsData.testImage),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(width: 30),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: const Text('Harry Potter and the Goblet for Fire',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Styles.textStyle20),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
