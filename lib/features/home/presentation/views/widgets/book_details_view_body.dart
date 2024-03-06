8838349fba3b0bc3fe7fa6bc64c959fad16db3e2
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-03-06 11:28:03 +0200

    add rating to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bokkly_app/core/utils/styles.dart';
+import 'package:bokkly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bokkly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bokkly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -28,6 +29,8 @@ class BookDetailsViewBody extends StatelessWidget {
                 style: Styles.textStyle18.copyWith(
                     fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
           ),
+          const SizedBox(height: 18),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
