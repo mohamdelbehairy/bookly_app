891a9d8a4995b8c7f49cda854bffc75326789b09
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-03-19 15:20:29 +0200

    newset books cubit done

--- lib/features/home/presentation/manager/newset_books/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_books/newset_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bokkly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bokkly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fethNewestBooks();
+
+    result.fold((failure) {
+      emit(NewsetBooksFailure(errorMessage: failure.errorMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books: books));
+    });
+  }
 }
